1cb5953800f5e159de2d1673251b16defd1cf980
Akherousin <akherousin94@gmail.com>
2023-04-10 04:28:03 +0600

    fix animation

--- src/components/MobileHeader/MobileHeader.jsx
+++ src/components/MobileHeader/MobileHeader.jsx
@@ -10,46 +10,54 @@ function MobileHeader() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className={styles.header}>
-      <Logo />
-      <button
-        className={`${styles.menu__button} click-target-helper`}
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-        aria-expanded={isMenuOpen}
-      >
-        <span className="visually-hidden">
-          {isMenuOpen ? 'Close menu' : 'Open menu'}
-        </span>
-        <img alt="" src={isMenuOpen ? close : menu} />
-      </button>
-
+    <>
       {isMenuOpen && (
-        <div className={styles.menu} aria-labelledby="menu-button">
-          <div className={styles.menu__content}>
-            <nav className={styles.nav}>
-              <ul className={styles.nav__list}>
-                <li>
-                  <Link to="/stories" className={styles.nav__link}>
-                    Stories
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/features" className={styles.nav__link}>
-                    Features
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/pricing" className={styles.nav__link}>
-                    Pricing
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-            <Button>Get an Invite</Button>
-          </div>
-        </div>
+        <div
+          className={styles.overlay}
+          onClick={() => setIsMenuOpen(false)}
+        ></div>
       )}
-    </header>
+      <header className={styles.header}>
+        <Logo />
+        <button
+          className={`${styles.menu__button} click-target-helper`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          aria-expanded={isMenuOpen}
+        >
+          <span className="visually-hidden">
+            {isMenuOpen ? 'Close menu' : 'Open menu'}
+          </span>
+          <img alt="" src={isMenuOpen ? close : menu} />
+        </button>
+
+        {isMenuOpen && (
+          <div className={styles.menu} aria-labelledby="menu-button">
+            <div className={styles.menu__content}>
+              <nav className={styles.nav}>
+                <ul className={styles.nav__list}>
+                  <li>
+                    <Link to="/stories" className={styles.nav__link}>
+                      Stories
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/features" className={styles.nav__link}>
+                      Features
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/pricing" className={styles.nav__link}>
+                      Pricing
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              <Button>Get an Invite</Button>
+            </div>
+          </div>
+        )}
+      </header>
+    </>
   );
 }
 
