2bb5e0ba216812546c02584e05ae9c39c5221bd0
Akherousin <akherousin94@gmail.com>
2023-04-10 00:30:27 +0600

    add react router

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,20 @@
+import Button from '../Button';
+import logo from '../../assets/shared/desktop/logo.svg';
+
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header aria-label="Site header">
+      <a>
+        <img src={logo} alt="photosnap logo" aria-hidden="true" />
+      </a>
+      <nav role="navigation" aria-label="Main navigation">
+        <a>Stories</a>
+        <a>Features</a>
+        <a>Pricing</a>
+      </nav>
+      <Button>Get an invite</Button>
+    </header>
+  );
 }
 
 export default Header;
