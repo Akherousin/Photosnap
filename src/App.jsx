3fd765ccad53c5ae4e93288f1359931af74b26ee
Akherousin <akherousin94@gmail.com>
2023-04-10 00:14:44 +0600

    fix obligatory react imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import React from 'react';
-import Button from './components/Button';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Button>Button 1</Button>
-      <Button variant="light">Button 1</Button>
-      <Button hasIcon>Button 1</Button>
-      <Button variant="light" hasIcon>
-        Button 1
-      </Button>
+      <Header />
     </div>
   );
 }
