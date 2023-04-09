0f88adc9d46bc72e37b2c43e6357682b7da6d218
Akherousin <akherousin94@gmail.com>
2023-04-09 21:35:01 +0600

    add css variables and utility classes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.scss';
+import './reset.scss';
 import './variables.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
