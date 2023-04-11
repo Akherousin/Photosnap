f7813d5951e12af738e169d84924cdcfc8e96e20
Akherousin <akherousin94@gmail.com>
2023-04-11 06:39:59 +0600

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
 import SuperHeader from './components/SuperHeader';
-import Hero from './components/Hero';
+import Section from './components/Section';
 import Image from './components/Image';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <SuperHeader />
-        <Hero
+        <Section
+          hasDecoration
           image={
             <Image
               pageName="home"
@@ -16,7 +18,11 @@ function App() {
               alt="A photographer standing on a bridge looking at a lake with mountains in the background"
             />
           }
-          hasButton
+          button={
+            <Button variant="light" hasIcon>
+              Get an invite
+            </Button>
+          }
         >
           <h1 className="heading-large">
             Create and share your photo stories.
@@ -26,7 +32,52 @@ function App() {
             We make it easy to share photos, tell stories and connect with
             others.
           </p>
-        </Hero>
+        </Section>
+        <Section
+          variant="light"
+          isReversed
+          image={
+            <Image
+              pageName="home"
+              imageName="beautiful-stories"
+              alt="A photographer standing on a bridge looking at a lake with mountains in the background"
+            />
+          }
+          button={
+            <Button variant="dark" hasIcon>
+              View the stories
+            </Button>
+          }
+        >
+          <h2 className="heading-large">Beautiful stories every&nbsp;time</h2>
+          <p className="main-text">
+            We provide design templates to ensure your stories look terrific.
+            Easily add photos, text, embed maps and media from other networks.
+            Then share your story with everyone.
+          </p>
+        </Section>
+        <Section
+          variant="light"
+          image={
+            <Image
+              pageName="home"
+              imageName="designed-for-everyone"
+              alt="A photographer standing on a bridge looking at a lake with mountains in the background"
+            />
+          }
+          button={
+            <Button variant="dark" hasIcon>
+              View the stories
+            </Button>
+          }
+        >
+          <h2 className="heading-large">Designed for everyone</h2>
+          <p className="main-text">
+            Photosnap can help you create stories that resonate with your
+            audience. Our tool is desigbed for photographers of all levels,
+            brands, businesses you name it.
+          </p>
+        </Section>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,47 @@
+import classNames from 'classnames';
+
+import './Section.scss';
+const possibleVariants = ['dark', 'light'];
+
+function Section({
+  image,
+  button,
+  variant = 'dark',
+  isHero,
+  isReversed,
+  hasDecoration,
+  children,
+}) {
+  if (typeof variant !== 'string' || !possibleVariants.includes(variant)) {
+    throw new Error(
+      'Invalid prop: variant must be a string of either "dark" or "light"'
+    );
+  }
+
+  const sectionClasses = classNames('section', `section--${variant}`, {
+    'section--reversed': isReversed,
+  });
+
+  return (
+    <section
+      className={sectionClasses}
+      style={{
+        '--max-section-height': isHero ? 'calc(650rem/16)' : 'calc(610rem/16)',
+      }}
+    >
+      <div className="section__content">
+        {hasDecoration && <div className="decoration" aria-hidden={true} />}
+        <div className="wrapper">
+          {children}
+          {button}
+          {hasDecoration && (
+            <div className="decoration--top" aria-hidden={true} />
+          )}
+        </div>
+      </div>
+      {image}
+    </section>
+  );
+}
+
+export default Section;
