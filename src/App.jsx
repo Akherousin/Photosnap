3135eb5f693c279325e469e5ab442435d065de52
Akherousin <akherousin94@gmail.com>
2023-04-13 06:49:56 +0600

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SuperHeader from './components/SuperHeader';
 import Section from './components/Section';
 import Image from './components/Image';
 import Button from './components/Button';
+import Stories from './components/Stories';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <SuperHeader />
         <Section
           hasDecoration
+          isHero
           image={
             <Image
               pageName="home"
@@ -78,6 +80,52 @@ function App() {
             brands, businesses you name it.
           </p>
         </Section>
+        <Stories>
+          <Stories.Article
+            image={
+              <Image pageName="stories" imageName="mountains" noTabletSizes />
+            }
+          >
+            <time>April 16th 2020</time>
+            <h2>The Mountains</h2>
+            <p>by John Appleseed</p>
+          </Stories.Article>
+          <Stories.Article
+            image={
+              <Image pageName="stories" imageName="cityscapes" noTabletSizes />
+            }
+          >
+            <time>April 14th 2020</time>
+            <h2>Sunset Cityscapes</h2>
+            <p>by Benjamin Cruz</p>
+          </Stories.Article>
+          <Stories.Article
+            image={
+              <Image
+                pageName="stories"
+                imageName="18-days-voyage"
+                noTabletSizes
+              />
+            }
+          >
+            <time>April 11th 2020</time>
+            <h2>18 Days Voyage</h2>
+            <p>by Alexei Borodin</p>
+          </Stories.Article>
+          <Stories.Article
+            image={
+              <Image
+                pageName="stories"
+                imageName="architecturals"
+                noTabletSizes
+              />
+            }
+          >
+            <time>April 9th 2020</time>
+            <h2>Architecturals</h2>
+            <p>by Samantha Brooke</p>
+          </Stories.Article>
+        </Stories>
       </BrowserRouter>
     </div>
   );
