b040b358e43c333f28c6f578559ca0dc6a1e1004
Arthur Badin <akherousin94@gmail.com>
2023-07-09 18:15:06 +0600

    refactor: extract data to another file

---
+++ src/DATA.js
@@ -0,0 +1,138 @@
+import responsiveSvg from './assets/features/desktop/responsive.svg';
+import noLimitSvg from './assets/features/desktop/no-limit.svg';
+import embedSvg from './assets/features/desktop/embed.svg';
+import curstomDomainSvg from './assets/features/desktop/custom-domain.svg';
+import boostExposureSvg from './assets/features/desktop/boost-exposure.svg';
+import dragDropSvg from './assets/features/desktop/drag-drop.svg';
+
+export const storiesList = [
+  {
+    title: 'The Mountains',
+    author: 'John Appleseed',
+    date: 'April 16th 2020',
+    image: 'mountains',
+  },
+  {
+    title: 'Sunset Cityscapes',
+    author: 'Benjamin Cruz',
+    date: 'April 14th 2020',
+    image: 'cityscapes',
+  },
+  {
+    title: '18 Days Voyage',
+    author: 'Alexei Borodin',
+    date: 'April 11th 2020',
+    image: '18-days-voyage',
+  },
+  {
+    title: 'Architecturals',
+    author: 'Samantha Brooke',
+    date: 'April 9th 2020',
+    image: 'architecturals',
+  },
+  {
+    title: 'World Tour 2019',
+    author: 'Timothy Wagner',
+    date: ' April 7th 2020',
+    image: 'world-tour',
+  },
+  {
+    title: 'Unforeseen Corners',
+    author: 'William Malcolm',
+    date: ' April 3rd 2020',
+    image: 'unforeseen-corners',
+  },
+  {
+    title: 'King on Africa: Part II',
+    author: 'Tim Hillenburg',
+    date: 'March 29th 2020',
+    image: 'king-on-africa',
+  },
+  {
+    title: 'The Trip to Nowhere',
+    author: 'Felicia Rourke',
+    date: 'March 21st 2020',
+    image: 'trip-to-nowhere',
+  },
+  {
+    title: 'Rage of The Sea',
+    author: 'Mohammed Abdul',
+    date: 'March 19th 2020',
+    image: 'rage-of-the-sea',
+  },
+  {
+    title: 'Running Free',
+    author: 'Michelle',
+    date: 'March 16th 2020',
+    image: 'running-free',
+  },
+  {
+    title: 'Behind the Waves',
+    author: 'Lamarr Wilson',
+    date: 'March11th2020',
+    image: 'behind-the-waves',
+  },
+  {
+    title: 'Calm Waters',
+    author: 'Samantha Brooke',
+    date: 'March9th2020',
+    image: 'calm-waters',
+  },
+  {
+    title: 'The Milky Way',
+    author: 'Benjamin Cruz',
+    date: 'March5th2020',
+    image: 'milky-way',
+  },
+  {
+    title: 'Night at The Dark Forest',
+    author: 'Mohammed Abdul',
+    date: 'March4th2020',
+    image: 'dark-forest',
+  },
+  {
+    title: 'Somwarpet’s Beauty',
+    author: 'Michelle',
+    date: 'March1st2020',
+    image: 'somwarpet',
+  },
+  {
+    title: 'Land of Dreams',
+    author: 'William Malcolm',
+    date: 'February25th2020',
+    image: 'land-of-dreams',
+  },
+];
+
+export const featuresList = [
+  {
+    title: '100% responsive',
+    desc: "No matter which the device you're on, our site is fully responsive and stories look beautiful on any screen.",
+    icon: responsiveSvg,
+  },
+  {
+    title: 'no photo upload limit',
+    desc: 'Our tool has no limits on uploads or bandwidth. Freely upload in bulk and share all of your stories in one go.',
+    icon: noLimitSvg,
+  },
+  {
+    title: 'Available to Embed',
+    desc: 'Embed Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google Maps, and more.',
+    icon: embedSvg,
+  },
+  {
+    title: 'Custom Domain',
+    desc: ' With Photosnap subscriptions you can host your stories on your own domain. You can also remove our branding!',
+    icon: curstomDomainSvg,
+  },
+  {
+    title: 'Boost Your Exposure',
+    desc: 'Users that viewed your story or gallery can easily get notifed of new and featured stories with our built in mailing list.',
+    icon: boostExposureSvg,
+  },
+  {
+    title: 'Drag & Drop Image',
+    desc: 'Easily drag and drop your image and get beautiful shots everytime. No over the top tooling to add friction to creating stories.',
+    icon: dragDropSvg,
+  },
+];
---
+++ src/pages/FeaturesPage/FeaturesPage.jsx
@@ -0,0 +1,32 @@
+import Section from '../../components/Section';
+import Image from '../../components/Image';
+import Features from '../../components/Features/Features';
+import { featuresList } from '../../DATA';
+
+function FeaturesPage() {
+  return (
+    <>
+      <Section
+        hasDecoration
+        image={<Image pageName="features" imageName="hero" />}
+      >
+        <h1 className="heading-large">Features</h1>
+        <p className="main-text">
+          We make sure all of features are designed to be loved by every
+          aspiring and even professional photographers who wanted to share their
+          stories.
+        </p>
+      </Section>
+      <Features>
+        {featuresList.map(({ title, desc, icon }) => (
+          <Features.Card key={title} icon={<img src={icon} alt="" />}>
+            <h3 className="heading-small">{title}</h3>
+            <p className="main-text">{desc}</p>
+          </Features.Card>
+        ))}
+      </Features>
+    </>
+  );
+}
+
+export default FeaturesPage;
