a7b5b7fee756c2a25659503f1c5746fe93e63b98
Arthur Badin <akherousin94@gmail.com>
2023-07-09 18:15:38 +0600

    fix: minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from '/src/pages/HomePage';
 import StoriesPage from '/src/pages/StoriesPage';
+import FeaturesPage from '/src/pages/FeaturesPage';
 import Layout from './components/Layout';
+import Toggle from './components/Toggle/Toggle';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route path="" element={<HomePage />} />
             <Route path="stories" element={<StoriesPage />} />
+            <Route path="features" element={<FeaturesPage />} />
+            <Route path="button" element={<Toggle />} />
           </Route>
         </Routes>
       </BrowserRouter>
