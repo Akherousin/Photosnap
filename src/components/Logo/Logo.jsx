2f145991dad9d0413e1eaf1d0c3a5e401102961a
Arthur Badin <akherousin94@gmail.com>
2023-07-19 03:51:46 +0600

    fix: change accName computation

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,9 +5,15 @@ function Logo({ variant = 'dark' }) {
   const gradientId = React.useId();
 
   return (
-    <Link to="/" aria-labelledby="logo__title">
-      <svg xmlns="http://www.w3.org/2000/svg" width="170" height="16">
-        <title id="logo__title">Photosnap - Home</title>
+    <Link to="/">
+      <span className="visually-hidden">Photosnap - Home</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="170"
+        height="16"
+        aria-hidden="true"
+        focusable="false"
+      >
         <defs>
           <linearGradient
             id={gradientId}
