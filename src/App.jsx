87d8d397719dc2b04ee1ee18c7d4b5813c66c0f5
Arthur Badin <akherousin94@gmail.com>
2023-07-19 14:56:54 +0600

    feat: add scroll to top when changing url

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+import Layout from './components/Layout';
 import HomePage from '/src/pages/HomePage';
 import StoriesPage from '/src/pages/StoriesPage';
 import FeaturesPage from '/src/pages/FeaturesPage';
-import Layout from './components/Layout';
-import Toggle from './components/Toggle/Toggle';
 import PricingPage from './pages/PricingPage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="" element={<HomePage />} />
             <Route path="stories" element={<StoriesPage />} />
             <Route path="features" element={<FeaturesPage />} />
             <Route path="pricing" element={<PricingPage />} />
-            <Route path="button" element={<Toggle />} />
           </Route>
         </Routes>
       </BrowserRouter>
