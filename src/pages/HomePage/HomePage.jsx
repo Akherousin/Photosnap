21d91a582040e86fea2918639d2622671903c1c6
Arthur Badin <akherousin94@gmail.com>
2023-07-19 14:57:40 +0600

    fix: remove dot

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -30,7 +30,7 @@ function HomePage() {
           </StyledLink>
         }
       >
-        <h1 className="heading-large">Create and share your photo stories.</h1>
+        <h1 className="heading-large">Create and share your photo stories</h1>
         <p className="main-text">
           Photosnap is a platform for photographers and visual storytellers. We
           make it easy to share photos, tell stories and connect with others.
